9c9591cd91c11aa87f35da15fe2b40ccb00d700c
melaraj2 <melaraj@gmail.com>
2023-02-10 12:47:36 -0500

    Update nlb.tf

--- modules/nlb/nlb.tf
+++ modules/nlb/nlb.tf
@@ -17,6 +17,7 @@ data "aws_iam_policy_document" "s3_lb_write" {
 
   statement {
     actions = ["s3:PutObject"]
+    effect = "Allow"
     resources = ["${aws_s3_bucket.nlbAccessLogBucket.arn}/logs/AWSLogs/${data.aws_caller_identity.current.account_id}/*"]
 
     principals  {
