44321ed2e859f322016a8e04ddd8f644d6588be9
Abdiel Sanchez <asanchez@transactrx.com>
2024-03-26 11:51:15 -0400

    Create an object instead of separate variables for auto scaler

--- modules/ecs-service/ecs-service.tf
+++ modules/ecs-service/ecs-service.tf
@@ -155,90 +155,51 @@ resource "aws_lb_target_group" "nlbTargetGroup" {
   vpc_id = var.vpc_id
 }
 
-variable "max_capacity" {
-  description = "The maximum number of tasks the service should scale out to."
-  type        = number
-  default     = 10
-}
+variable "auto_scaler_config" {
+  type = object({
+    max_capacity = number
+    min_capacity = number
 
-variable "min_capacity" {
-  description = "The minimum number of tasks the service should scale in to."
-  type        = number
-  default     = 1
-}
+    enable_cpu_scaling = bool
+    cpu_scale_out_target_value = number
+    cpu_scale_in_target_value = number
+    cpu_scale_in_cooldown = number
+    cpu_scale_out_cooldown = number
 
-variable "enable_cpu_scaling" {
-  description = "Enable auto-scaling based on CPU utilization."
-  type        = bool
-  default     = false
-}
-
-variable "cpu_scale_out_target_value" {
-  description = "The target value for the scale out policy (e.g., CPU utilization)."
-  type        = number
-  default     = 80
-}
-
-variable "cpu_scale_in_target_value" {
-  description = "The target value for the CPU utilization to scale in."
-  type        = number
-  default     = 60
-}
-
-variable "cpu_scale_in_cooldown" {
-  description = "The cooldown period before allowing another scale in after the last one."
-  type        = number
-  default     = 120
-}
-
-variable "cpu_scale_out_cooldown" {
-  description = "The cooldown period before allowing another scale out after the last one."
-  type        = number
-  default     = 120
-}
-
-variable "enable_memory_scaling" {
-  description = "Enable auto-scaling based on Memory utilization."
-  type        = bool
-  default     = false
-}
-
-variable "mem_scale_out_target_value" {
-  description = "The target value for the scale out policy (e.g., Memory utilization)."
-  type        = number
-  default     = 80
-}
-
-variable "mem_scale_in_target_value" {
-  description = "The target value for the Memory utilization to scale in."
-  type        = number
-  default     = 60
-}
-
-variable "mem_scale_in_cooldown" {
-  description = "The cooldown period before allowing another scale in after the last one."
-  type        = number
-  default     = 120
-}
-
-variable "mem_scale_out_cooldown" {
-  description = "The cooldown period before allowing another scale out after the last one."
-  type        = number
-  default     = 120
+    enable_memory_scaling = bool
+    mem_scale_out_target_value = number
+    mem_scale_in_target_value = number
+    mem_scale_in_cooldown = number
+    mem_scale_out_cooldown = number
+  })
+  default = {
+    max_capacity               = 10
+    min_capacity               = 1
+    enable_cpu_scaling         = false
+    cpu_scale_out_target_value = 80
+    cpu_scale_in_target_value  = 60
+    cpu_scale_in_cooldown      = 120
+    cpu_scale_out_cooldown     = 120
+    enable_memory_scaling      = false
+    mem_scale_out_target_value = 80
+    mem_scale_in_target_value  = 60
+    mem_scale_in_cooldown      = 120
+    mem_scale_out_cooldown     = 120
+  }
 }
 
 resource "aws_appautoscaling_target" "ecs_service_target" {
-  count = var.enable_cpu_scaling || var.enable_memory_scaling ? 1 : 0
+  count = var.auto_scaler_config.enable_cpu_scaling || var.auto_scaler_config.enable_memory_scaling ? 1 : 0
 
-  max_capacity       = var.max_capacity
-  min_capacity       = var.min_capacity
+  max_capacity       = var.auto_scaler_config.max_capacity
+  min_capacity       = var.auto_scaler_config.min_capacity
   resource_id        = "service/${var.clusterName}/${var.serviceName}"
   scalable_dimension = "ecs:service:DesiredCount"
   service_namespace  = "ecs"
 }
 
 resource "aws_appautoscaling_policy" "ecs_average_cpu_scaling_policy" {
-  count              = var.enable_cpu_scaling ? 1 : 0
+  count              = var.auto_scaler_config.enable_cpu_scaling ? 1 : 0
   name               = "${var.serviceName}_average_cpu_scaling_policy"
   service_namespace  = aws_appautoscaling_target.ecs_service_target[count.index].service_namespace
   resource_id        = aws_appautoscaling_target.ecs_service_target[count.index].resource_id
@@ -249,14 +210,14 @@ resource "aws_appautoscaling_policy" "ecs_average_cpu_scaling_policy" {
     predefined_metric_specification {
       predefined_metric_type = "ECSServiceAverageCPUUtilization"
     }
-    target_value       = var.cpu_scale_out_target_value
-    scale_in_cooldown  = var.cpu_scale_in_cooldown
-    scale_out_cooldown = var.cpu_scale_out_cooldown
+    target_value       = var.auto_scaler_config.cpu_scale_out_target_value
+    scale_in_cooldown  = var.auto_scaler_config.cpu_scale_in_cooldown
+    scale_out_cooldown = var.auto_scaler_config.cpu_scale_out_cooldown
   }
 }
 
 resource "aws_appautoscaling_policy" "ecs_memory_scaling_policy" {
-  count              = var.enable_memory_scaling ? 1 : 0
+  count              = var.auto_scaler_config.enable_memory_scaling ? 1 : 0
   name               = "${var.serviceName}_memory_scaling_policy"
   service_namespace  = aws_appautoscaling_target.ecs_service_target[count.index].service_namespace
   resource_id        = aws_appautoscaling_target.ecs_service_target[count.index].resource_id
@@ -267,9 +228,9 @@ resource "aws_appautoscaling_policy" "ecs_memory_scaling_policy" {
     predefined_metric_specification {
       predefined_metric_type = "ECSServiceAverageMemoryUtilization"
     }
-    target_value       = var.mem_scale_in_target_value
-    scale_in_cooldown  = var.mem_scale_in_cooldown
-    scale_out_cooldown = var.mem_scale_out_cooldown
+    target_value       = var.auto_scaler_config.mem_scale_in_target_value
+    scale_in_cooldown  = var.auto_scaler_config.mem_scale_in_cooldown
+    scale_out_cooldown = var.auto_scaler_config.mem_scale_out_cooldown
   }
 }
 
