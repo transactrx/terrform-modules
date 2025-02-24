584b0af44e86cb3a39b0153e0c530fc62bda294d
melaraj2 <melaraj@gmail.com>
2025-02-24 13:52:12 -0500

    register dns for the dns name, and point it to the ALB

--- modules/ecs-service-with-alb/ecs-service-with-alb.tf
+++ modules/ecs-service-with-alb/ecs-service-with-alb.tf
@@ -55,8 +55,8 @@ resource "aws_route53_record" "app_dns" {
   type    = "A"
 
   alias {
-    name                   = data.alb.dns_name
-    zone_id                = data.alb.zone_id
+    name                   = data.aws_lb.alb.dns_name
+    zone_id                = data.aws_lb.alb.zone_id
     evaluate_target_health = false
   }
 }
